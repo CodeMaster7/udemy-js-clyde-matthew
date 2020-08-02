5722411bad28bcc0acd41bd0d7bbdf4c73888878
sam <sammybonfanti@gmail.com>
2020-08-02 16:43:53 -0600

    array literal array constructor

---
+++ section 12 - arrays/arrays1.js
@@ -0,0 +1,24 @@
+// ARRAY LITERAL (USE MORE)
+let a = []
+a;
+
+// create an array with values
+let animals = ['dog', 'cat', 'mouse']
+animals;
+
+let people = [
+    {name: 'wally'},
+    {name: 'john'},
+    {name: 'cindy'}
+]
+people;
+
+// ARRAY CONSTRUCTOR
+let house = new Array('3 bedroom', 'red', '3 bathrooms')
+house;
+
+let numbers = new Array(5, 10)
+numbers;
+
+let figures = new Array(7)
+figures
