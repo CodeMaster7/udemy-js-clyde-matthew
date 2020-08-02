9ff8c3aaa96aeae7d9d5dc2b3aa696ee683644f0
sam <sammybonfanti@gmail.com>
2020-08-02 16:44:01 -0600

    push and pop

---
+++ section 12 - arrays/arrays2.js
@@ -0,0 +1,21 @@
+// create a shopping list
+let shoppingList = ['milk', 'bread', 'beer']
+shoppingList;
+
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+
+// lets override values
+shoppingList[0] = 'Coke'
+shoppingList;
+
+// accesss the length property
+console.log(shoppingList.length);
+
+// add an item to the end of our shopping list
+shoppingList.push('toilet paper')
+shoppingList;
+
+// remove last item from the array
+shoppingList.pop()
+shoppingList;
