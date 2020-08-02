5ef2edc37dd310b985cad6c6e551ea5c21f8eb50
sam <sammybonfanti@gmail.com>
2020-08-02 14:09:40 -0600

    classes es2015

---
+++ section 11 - objects/objectClasses1.js
@@ -0,0 +1,29 @@
+// CLASSES
+
+// 1. constructor function
+function Person (name) {
+    this.name = name
+}
+
+Person.prototype.greeting = function () {
+    console.log(this.name);
+}
+
+let user = new Person('wally')
+user.greeting()
+
+// 2. class syntax
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    greeting() {
+        console.log(this.name);
+    }
+}
+
+let user1 = new Person('wally1')
+user1.greeting()
+
+// in Javascript, a class is a kind of function, and it puts the methods we create here into the prototype property
+console.log(Person);
